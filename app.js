306e3094a04f9be8d9c8e1fd60c15c4d8152bae8
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-10-03 18:04:06 -0700

    add home route

--- app.js
+++ app.js
@@ -18,3 +18,15 @@ mongoose
   .catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const index = require('./routes/index');
+app.use('/', index);
